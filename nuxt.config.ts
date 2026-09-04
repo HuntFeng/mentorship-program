b27f9c2bc2792a2a1728295a59ef2b14749833d7
HuntFeng <spacecurvature@gmail.com>
2026-09-03 22:40:52 -0600

    added nuxt ui

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -2,7 +2,7 @@
 export default defineNuxtConfig({
   compatibilityDate: '2025-07-15',
   devtools: { enabled: true },
-  modules: ['@nuxt/scripts'],
+  modules: ['@nuxt/ui', '@nuxt/scripts'],
   runtimeConfig: {
     stripeSecretKey: process.env.STRIPE_SECRET_KEY,
     public: {
