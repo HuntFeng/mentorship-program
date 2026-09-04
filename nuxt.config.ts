d409df0cb59bb90047bc22221162ea9aed92acc6
HuntFeng <spacecurvature@gmail.com>
2026-09-04 01:20:46 -0600

    don't auto redirect to login page

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -9,6 +9,7 @@ export default defineNuxtConfig({
   ],
   css: ['~/assets/css/main.css'],
   supabase: {
+    redirect: false,
     url: process.env.NUXT_PUBLIC_SUPABASE_URL || '',
     key: process.env.NUXT_PUBLIC_SUPABASE_KEY || '',
     secreteKey: process.env.NUXT_SUPABASE_SECRET_KEY || '',
