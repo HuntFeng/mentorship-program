4abf16d2cdcfe57ccfa39df43ea801289377dbad
HuntFeng <spacecurvature@gmail.com>
2026-09-03 23:29:08 -0600

    remove stripe keys

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -3,11 +3,5 @@ export default defineNuxtConfig({
   compatibilityDate: '2025-07-15',
   devtools: { enabled: true },
   modules: ['@nuxt/ui', '@nuxt/scripts'],
-  runtimeConfig: {
-    stripeSecretKey: process.env.STRIPE_SECRET_KEY,
-    public: {
-      stripeProductId: process.env.STRIPE_PRODUCT_ID,
-      stripePublishableKey: process.env.NUXT_PUBLIC_STRIPE_PUBLISHABLE_KEY
-    }
-  }
+  css: ['~/assets/css/main.css'],
 })
