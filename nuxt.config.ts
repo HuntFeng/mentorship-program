1253c2cf2ae8c0a9416d088d0b32241787c0e67f
HuntFeng <spacecurvature@gmail.com>
2026-09-05 00:47:35 -0600

    remove baseURL

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,8 +1,5 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-  app: {
-    baseURL: '/mentorship-program/'
-  },
   compatibilityDate: '2025-07-15',
   devtools: { enabled: true },
   modules: [
